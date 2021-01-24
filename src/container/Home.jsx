e83fad5ab69255dcc7f5ba39216b85e43667d28a
chris kim <chriskim0926@gmail.com>
2021-01-23 21:14:41 -0500

    Sorting added

--- src/container/Home.jsx
+++ src/container/Home.jsx
@@ -25,7 +25,41 @@ const Home = () => {
     console.log(e.target.value)
     setInput(e.target.value);
   };
- 
+
+  function sortTable() {
+    var table, rows, switching, i, x, y, shouldSwitch;
+    table = document.getElementById("myTable");
+    switching = true;
+    /*Make a loop that will continue until
+    no switching has been done:*/
+    while (switching) {
+      //start by saying: no switching is done:
+      switching = false;
+      rows = table.rows;
+      /*Loop through all table rows (except the
+      first, which contains table headers):*/
+      for (i = 1; i < (rows.length - 1); i++) {
+        //start by saying there should be no switching:
+        shouldSwitch = false;
+        /*Get the two elements you want to compare,
+        one from current row and one from the next:*/
+        x = rows[i].getElementsByTagName("TD")[0];
+        y = rows[i + 1].getElementsByTagName("TD")[0];
+        //check if the two rows should switch place:
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          //if so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+        }
+      }
+      if (shouldSwitch) {
+        /*If a switch has been marked, make the switch
+        and mark that a switch has been done:*/
+        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+        switching = true;
+      }
+    }
+  }
  
 
   return (
@@ -43,12 +77,12 @@ const Home = () => {
       />
         </div>
         <div className="row">
-          <table className="table">
+          <table id="myTable">
             <thead>
               <tr>
                 <th scope="col">Image</th>
-                <th scope="col">Name</th>
-                <th scope="col">Location</th>
+                <th scope="col" ><button onClick = {sortTable}>Sort Alphabetically</button> Name</th>
+                <th scope="col" >Location</th>
                 <th scope="col">Email</th>
                 <th scope="col">Phone</th>
               </tr>
@@ -56,7 +90,7 @@ const Home = () => {
             
             {employees.filter((val) => {
               
-              if (input ==""){
+              if (input ===""){
                 return val
               }else if (val.name.first.toLowerCase().includes(input.toLowerCase())){
 
@@ -64,7 +98,7 @@ const Home = () => {
 
               }else if (val.name.last.toLowerCase().includes(input.toLowerCase())){
                 return val
-                
+          
               }
 
             }).map((employee)=>(
@@ -95,6 +129,7 @@ const Home = () => {
         </div>
       </div>
     </div>
+    
   );
 };
 
